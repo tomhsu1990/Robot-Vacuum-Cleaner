3bbc5bac427dcd692a161e462e6cdbde0825dc54
Ching-Hsiang (Tom) Hsu <chhsu@nyu.edu>
2018-03-03 17:24:29 -0500

    visited small cc

--- Cleaner.cpp
+++ Cleaner.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <queue>
 #include <deque>
 #include <unordered_map>
 
@@ -51,7 +52,7 @@ public:
 	}
 
 	bool Move (bool touchWall) {
-		// for visualization
+		// for animation visualization
 		//if (touchWall){
 			//std::cout << "move " << glb_pos.first << " " << glb_pos.second << " " << dir << std::endl;
 			//mp[glb_pos.first][glb_pos.second] = '*';
@@ -110,6 +111,43 @@ public:
 		dir = (dir+k)%4;
 	}
 
+	std::vector<Location> bfs (Location cur, Location nxt) {
+		Location start(cur);
+		std::vector<Location> route;
+		std::unordered_map<std::string, Location> visited;
+		std::queue<Location> q;
+		bool find(false);
+		int dx[] = {-1,0,1,0};
+		int dy[] = {0,1,0,-1};
+
+		q.push(cur);
+		visited[std::to_string(cur.first)+","+std::to_string(cur.second)+","] = cur;
+		while (!q.empty() && !find) {
+			cur = q.front(); q.pop();
+			for (int i=0;i<4;++i) {
+				Location tmp(cur.first+dx[i], cur.second+dy[i]);
+				std::string p(std::to_string(tmp.first)+","+std::to_string(tmp.second)+",");
+				if (cleaned.find(p) != cleaned.end() && 
+					visited.find(p) == visited.end()){
+					visited[p] = cur;
+					if (tmp == nxt) {
+						find = true;
+						break;
+					}
+					q.push(tmp);
+				}
+			}
+		}
+		if (find) {
+			do {
+				route.push_back(nxt);
+				nxt = visited[std::to_string(nxt.first)+","+std::to_string(nxt.second)+","];
+			} while (nxt != start);
+		}
+		return route;
+	}
+
+	// do we have to keep a dynamic local map?
 	void updateLocalMap () {
 		if (dir == N) {
 			pos.push_back(Cleaner::Location(glb_pos.first+1,glb_pos.second));
@@ -125,7 +163,7 @@ public:
 		}
 	}
 
-	void updateGlocalMap () {
+	void updateGlobalMap () {
 		for (auto it=cleaned.begin();it!=cleaned.end();++it) {
 			std::vector<int> loc = parseLocation(it->first);
 			mp[loc[0]][loc[1]] = 'x'; // x means cleaned.
@@ -135,7 +173,7 @@ public:
 	void clean () {
 		// initialize robot local pose
 		locl_pos = Cleaner::Location(0,0);
-		//do {
+		do {
 			// turn the direction to N
 			while(dir != N) turnRight(1);
 			// move forward and touch a wall
@@ -161,29 +199,34 @@ public:
 			}
 
 			// go to a possible un-cleaned position
-			// bool find(false);
-			// Location nxt;
-			// while (true) {
-			// 	if (pos.empty()) break;
-			// 	else {
-			// 		nxt = pos.front(); pos.pop_front();
-			// 		if (nxt.first < 0 || nxt.first >= mp.size() || 
-			// 			nxt.second < 0 || nxt.second >= mp[nxt.first].size() ||
-			// 			mp[nxt.first][nxt.second] == '+')
-			// 			continue;
-			// 		if (cleaned.find(std::to_string(nxt.first)+","+std::to_string(nxt.second)+",") != cleaned.end())
-			// 			continue;
-			// 		find = true;
-			// 		break;
-			// 	}
-			// }
-			// if (find) {
-			// 	// move to nxt as the start of the next round
+			bool find(false);
+			Location nxt;
+			while (true) {
+				if (pos.empty()) break;
+				else {
+					nxt = pos.front(); pos.pop_front();
+					if (nxt.first < 0 || nxt.first >= mp.size() || 
+						nxt.second < 0 || nxt.second >= mp[nxt.first].size() ||
+						mp[nxt.first][nxt.second] == '+')
+						continue;
+					if (cleaned.find(std::to_string(nxt.first)+","+std::to_string(nxt.second)+",") != cleaned.end())
+						continue;
+					find = true;
+					break;
+				}
+			}
+			if (find) {
+				// move to nxt as the start of the next round
+				// via previous exploring positions
+				std::vector<Location> route = bfs(glb_pos, nxt);
+				if (route.empty()) break;
+				for (int i=route.size()-1;i>=0;--i) {
+					
+				}
+			}
+		} while (!pos.empty()) ;
 
-			// }
-		//} while (!pos.empty()) ;
-
-		updateGlocalMap();
+		updateGlobalMap();
 	}
 
 	void showGlobalMap () {
@@ -211,8 +254,8 @@ public:
 
 	int dir;
 	Location glb_pos, locl_pos;
-	std::vector<std::string> mp;
-	std::deque<std::deque<char>> dy_mp;
+	std::vector<std::string> mp; 		// global map
+	std::deque<std::deque<char>> dy_mp; // local map
 	std::unordered_map<std::string, int> vis;
 	std::unordered_map<std::string, int> cleaned;
 	// check whether it is visted
